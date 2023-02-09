a9f54abaaee1e99b145d8993fb5ca24614caa8fc
Harrison Kaiser <uwaces@gmail.com>
2023-02-09 16:39:12 -0500

    Remove debug

--- codebuild/bin/s2n_codebuild.sh
+++ codebuild/bin/s2n_codebuild.sh
@@ -95,10 +95,8 @@ run_integration_v2_tests() {
     cd build/
     for test_name in $TOX_TEST_NAME; do
       test="${test_name//test_/}"
-      set +x
-      #echo "Running... cmake --build build/ --target test -- ARGS=\"--output-on-failure --verbose --no-tests=fail -R ^integrationv2_${test}$\""
-      ctest --show-only --output-on-failure --verbose --extra-verbose --debug --no-tests=fail -R ^integrationv2_${test}$
-      #cmake --build build/ --target test -- ARGS="--show-only --output-on-failure --verbose --extra-verbose --debug --no-tests=fail -R ^integrationv2_${test}$"
+      echo "Running... ctest --show-only --output-on-failure --verbose --extra-verbose --debug --no-tests=fail -R ^integrationv2_${test}$"
+      ctest --output-on-failure --verbose --no-tests=fail -R ^integrationv2_${test}$
     done
 }
 
